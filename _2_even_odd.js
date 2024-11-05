ee6f84f5a70487b5160119e472ab8f8210254ba8
deshmukhmahima32 <deshmukhmahima40@gmail.com>
2024-11-05 13:46:14 +0530

    Add files via upload

---
+++ _2_even_odd.js
@@ -0,0 +1,11 @@
+const prompt=require('prompt-sync')();
+
+let num=parseInt(prompt(`enter a number `))
+if(num%2==0)
+{
+    console.log(`${num} is even`)
+}
+else{
+    console.log(`${num} is odd`)
+    
+}
