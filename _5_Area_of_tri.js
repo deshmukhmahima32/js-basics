73bdbc5c683ee1da5b246faf1470e03941795734
deshmukhmahima32 <deshmukhmahima40@gmail.com>
2024-11-06 13:37:50 +0530

    Add files via upload

---
+++ _5_Area_of_tri.js
@@ -0,0 +1,7 @@
+const prompt=require('prompt-sync')();
+
+const base=prompt(`enter base of tringle `)
+const height=prompt(`enter height of triangle `)
+let trianglearea=(1/2)*base*height;
+console.log(`area of tringle is ${trianglearea}`)
+
